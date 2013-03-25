7d72ed9665780a9c968abcb17960172d26f35b72
Weixin Chen <weixinchen@Weixin-Chens-MacBook-Pro.local>
2013-03-25 17:06:55 -0400

    weixin

--- src/player/Token.java
+++ src/player/Token.java
@@ -34,7 +34,7 @@ public class Token {
 	 */
 
 	public static enum Type {
-		M, C, K, L,Q, T, X, V, Rest, Pitch, Tuplets, ChordsBegin, ChordsEnd, Barline, RepeatBegin, RepeatEnd, Nrepeat
+		M, C, K, L,Q, T, X, V, Rest, Pitch, Tuplets, ChordsBegin, ChordsEnd, Barline, RepeatBegin, RepeatEnd, Repeat_first, Repeat_second
 	}
 
 	public final Type type;
@@ -44,138 +44,82 @@ public class Token {
 	public double noteLength;
 	public int octave;
 	public int accid;
-	
+	public int chord;
 	
 	/**
 	 * Method Token converts regular expressions which are used as grammars to strings
 	 * @param type: a regex which belongs to the types of regex defined in enum.
 	 * string: the string expression of the regex
 	 */
-
-	public Token(Type type, String string, String basenote, double noteLength, int octave, int accid) {
+	
+	public Token(Type type, String string, String basenote, double noteLength, int octave, int accid, int chord) {
 		this.type = type;
 		switch (type) {
 		case M:
 			this.pattern = Pattern.compile("M:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case K:
 			this.pattern = Pattern.compile("K:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case T:
 			this.pattern = Pattern.compile("T:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case L:
 			this.pattern = Pattern.compile("L:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case Q:
 			this.pattern = Pattern.compile("Q:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case C:
 			this.pattern = Pattern.compile("C:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case X:
 			this.pattern = Pattern.compile("X:.*");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case V:
-			this.pattern = Pattern.compile("V:d+");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
+			this.pattern = Pattern.compile("V:d+"); 
 			break;
 		case Rest:
 			this.pattern = Pattern.compile("z [[d* /? d+] | /]?");
 			this.basenote = "z";
 			this.noteLength = 0.0;
+			//other choice?
 			this.octave = 0;
-			this.accid = 0; 
+			this.accid = 0;
+			this.chord = 0;
 			break;
 		case Pitch:
 			this.pattern = Pattern.compile("[\\^ | \\^\\^ | _ | __ | =]? [A-Ga-g] ['+ ,+]? [[d* /? d+] | /]?");
 			this.basenote = Pattern.compile("[A-Ga-g]").toString();
 			this.noteLength = 0.0;
 			this.octave = 0;
-			this.accid = 0; 
-			
+			this.accid = 0;
+			this.chord = 0;
 			break;
 		case Tuplets:
 			this.pattern = Pattern.compile("\\([234]");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case ChordsBegin:
-			this.pattern = Pattern.compile("\\[");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
+			this.pattern = Pattern.compile("\\["); 
 			break;
 		case ChordsEnd:
 			this.pattern = Pattern.compile("\\]");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
 		case Barline:
-			this.pattern = Pattern.compile("\\| |  \\|\\| | \\[\\| | \\|\\]");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
+			this.pattern = Pattern.compile("\\| |  \\|\\| | \\[\\| | \\|\\]"); 
 			break;
 		case RepeatBegin:
 			this.pattern = Pattern.compile("\\|:");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
-
 		case RepeatEnd:
 			this.pattern = Pattern.compile(":\\|");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
 			break;
-		case Nrepeat:
-			this.pattern = Pattern.compile("\\[[12]");
-			this.basenote = "0";
-			this.noteLength = 0.0;
-			this.octave = 0;
-			this.accid = 0; 
+		case Repeat_first:
+			this.pattern = Pattern.compile("\\[1"); 
 			break;
+        case Repeat_second:
+            this.pattern = Pattern.compile("\\[2"); 
+            break;		
 		default:
 			throw new RuntimeException("The input type is invalid");
 		}
