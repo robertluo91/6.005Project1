b3b6b86bbb1ed2f4938a35522ae8e909b8b58cf7
Yiyang Luo <yluo15@mit.edu>
2013-03-26 22:43:40 -0400

    robert

--- src/player/LexerTest.java
+++ src/player/LexerTest.java
@@ -5,34 +5,41 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import player.Lexer;
 import player.Token;
+import player.Token.Type;
 
 public class LexerTest {
-    /**
-     * Helper method for tests on good inputs
-     */
-    public void testHelper(Lexer l, List<Token> expected) {
-        List<Token> result = new ArrayList<Token>();
-        while (l.peek() != null) {
-            try {
-              result.add(l.next());
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
-          }
-          assertEquals(expected.size(), result.size());
-          for (int i = 0; i < expected.size(); i++) {
-              assertEquals(expected.get(i),result.get(i));
-          }
-    }
     
-    @Test
-    public void HeaderTest(){
-        String Expression = "M:4/4";
+	// step0: define token equality: both type and string must match
+    public static void assertTokenEquals(Token x, Token y) {
+    	
+        assertEquals(x.type, y.type);
+        assertEquals(x.string, y.string);
+        assertEquals(x.basenote, y.basenote);
+        assertEquals(x.noteLength, y.noteLength, x.noteLength);
+        assertEquals(x.octave, y.octave);
+        assertEquals(x.accid, y.accid);
+        assertEquals(x.chord, y.chord);
         
+
     }
-    
+
+    @Test
+    //Test Header
+    public void HeaderTest1() {
+        
+        Lexer lexer = new Lexer("piece1.abc");
+        ArrayList<Token> resultTokens = lexer.MusicHeader; 
+        Token[] expected = { new Token(Type.X, "X:1",0,0.0,0, 0,0),   
+                new Token(Type.T, "T: Piece No.1",0,0.0,0, 0,0), new Token(Type.M, "M:4/4",0,0.0,0, 0,0),
+                new Token(Type.L, "L:1/4",0,0.0,0, 0,0), new Token(Type.Q, "Q:140",0,0.0,0, 0,0),
+                new Token(Type.K, "K:C",0,0.0,0, 0,0) };   
+        for (int i=0;i<expected.length;i++){
+        	assertTokenEquals(resultTokens.get(i), expected[i]);
+        }
+
+    }
 }
+    
