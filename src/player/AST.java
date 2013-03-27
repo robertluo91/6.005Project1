f989bf9a280f1969dd3af7b8eccc841d6c212099
Weixin Chen <weixinchen@Weixin-Chens-MacBook-Pro.local>
2013-03-26 22:42:21 -0400

    weixin

--- src/player/AST.java
+++ src/player/AST.java
@@ -2,16 +2,16 @@ package player;
 
 import java.util.List; 
 
-public interface AST<E> {
-    public boolean hasChildren(); 
-    
-    public int numChildren(); 
-      
-    public E getValue(); 
-      
-    public AST<E> getChild(int index); 
-      
-    public List<AST<E>> getAllChildren(); 
+public interface AST<E>{
+    public interface Visitor<R> {
+        public R on(ParentTree p);
+ 
+        public R on(NodeTree n);
+    }
+
+    //public String toString();
+ 
+    public <R> R accept(Visitor<R> v);
     
     //recover after finishing visitor
     //public List<Note> accept(Visitor<E> visitor, int startTick); 
--- src/player/ParentTree.java
+++ src/player/ParentTree.java
@@ -1,45 +1,25 @@
 package player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ParentTree<E> implements AST<E>{
-    private final E value; 
-    final List<AST<E>> list;
-    public ParentTree(E _value, List<AST<E>> _list) { 
-        this.value = _value; 
-        this.list = _list; 
+    final NodeTree middle;
+    final NodeTree left;
+    final NodeTree right;
+        
+    public ParentTree(NodeTree middle, NodeTree left, NodeTree right) throws RuntimeException{
+        NodeTree currentMiddle = null;
+        NodeTree currentLeft = null;
+        NodeTree currentRight = null;
+        this.middle = current;
+        this.left = left;
+        this.right = right;
     }
-    public boolean hasChildren(){ 
-        return (list.size() > 0); 
-    } 
-      
-    public int numChildren(){ 
-        return list.size(); 
-    } 
-      
-    public void addChild(AST<E> abc){ 
-        this.list.add(abc); 
-    } 
-      
-    public void addAllChildren(List<AST<E>> l){ 
-        this.list.addAll(l); 
-    } 
-           
-    public E getValue(){ 
-        return value; 
-    } 
+ 
+    public <R> R accept(Visitor<R> v) {
+        return v.on(this);
+    }
     
-    public AST<E> getChild(int index){ 
-        return list.get(index); 
-    } 
-      
-    public List<AST<E>> getAllChildren(){ 
-        return new ArrayList<AST<E>>(list); 
-    }
-    //recover after finishing visitor
-    //@Override
-    //public List<Note> accept(Visitor<E> visitor, int startTick){ 
-    //    throw new RuntimeException("cannot be implemented"); 
-    //}
+
 }
+
