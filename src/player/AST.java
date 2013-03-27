15a48bd291dc7f16b12ef67ba42b850d4a54b0a3
ytong1015 <ytong@wellesley.edu>
2013-03-26 22:41:11 -0400

    ytong

--- src/player/AST.java
+++ src/player/AST.java
@@ -1,18 +1,7 @@
 package player;
 
-import java.util.List; 
 
 public interface AST<E> {
-    public boolean hasChildren(); 
-    
-    public int numChildren(); 
-      
-    public E getValue(); 
-      
-    public AST<E> getChild(int index); 
-      
-    public List<AST<E>> getAllChildren(); 
-    
-    //recover after finishing visitor
-    //public List<Note> accept(Visitor<E> visitor, int startTick); 
+     
+
 }
